818e7393340767232a5f3296ce5d73186eaf909b
ray <397593251@qq.com>
2022-10-20 15:51:54 +0800

    add pointcloud

--- pointcloud.py
+++ pointcloud.py
@@ -19,3 +19,35 @@ def visualize_pc(pcd):
     vis.add_geometry(pcd)
     vis.run()
     vis.destroy_window()
+
+
+def load_pointcloud(file_path):
+    file_format = file_path.split('.')[-1]
+    if file_format == 'asc':
+        # detect skip rows for split xyz,rgb
+        skiprows = 0
+        with open(file_path, 'r') as txt_file:
+            while True:
+                line = txt_file.readline()
+                space_count = len(line.split(' ')) - 1
+                if line:
+                    if line[0] == '/' or (space_count != 8 and space_count != 9):
+                        skiprows += 1
+                    if line[0] == '/' or (space_count != 8 and space_count != 9):
+                        break
+                else:
+                    break
+
+        xyz_rgb = pd.read_csv(file_path, sep=' ', header=None, skiprows=skiprows,
+                              usecols=[i for i in range(6)]).values
+    elif file_format == 'pth':
+        xyz, rgb, semantic_label, instance_label = torch.load(file_path)
+        rgb = (rgb + 1) * 127.5
+        xyz_rgb = np.hstack((xyz, rgb)).astype(np.float32)
+    else:
+        pc = o3d.io.read_point_cloud(file_path)
+        xyz = np.array(pc.points)
+        rgb = np.array(pc.colors) * 255
+        xyz_rgb = np.hstack((xyz, rgb)).astype(np.float32)
+
+    return xyz_rgb
