56195c731310d0bd0daf00a3fbac2f31fd6a0d09
ray <397593251@qq.com>
2022-10-27 14:48:01 +0800

    add pointcloud

--- pointcloud.py
+++ pointcloud.py
@@ -51,3 +51,55 @@ def load_pointcloud(file_path):
         xyz_rgb = np.hstack((xyz, rgb)).astype(np.float32)
 
     return xyz_rgb
+
+
+def RemoveNoiseStatistical(pc, nb_neighbors=20, std_ratio=2.0):
+    """ remove point clouds noise using statitical noise removal method
+
+    Args:
+        pc (ndarray): N x 3 point clouds
+        nb_neighbors (int, optional): Defaults to 20.
+        std_ratio (float, optional): Defaults to 2.0.
+
+    Returns:
+        [ndarray]: N x 3 point clouds
+    """
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pc)
+    cl, ind = pcd.remove_statistical_outlier(
+        nb_neighbors=nb_neighbors, std_ratio=std_ratio)
+
+    return np.asarray(cl.points)
+
+
+def DetectMultiPlanes(points, min_ratio=0.05, threshold=0.01, iterations=1000):
+    """ Detect multiple planes from given point clouds
+
+    Args:
+        points (np.ndarray):
+        min_ratio (float, optional): The minimum left points ratio to end the Detection. Defaults to 0.05.
+        threshold (float, optional): RANSAC threshold in (m). Defaults to 0.01.
+
+    Returns:
+        [List[tuple(np.ndarray, List)]]: Plane equation and plane point index
+    """
+
+    plane_list = []
+    N = len(points)
+    target = points.copy()
+    count = 0
+
+    while count < (1 - min_ratio) * N:
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(target)
+        w, index = pcd.segment_plane(
+            threshold, 3, iterations)
+        # w, index = PlaneRegression(
+        #     target, threshold=threshold, init_n=3, iter=iterations)
+
+        count += len(index)
+        plane_list.append((w, target[index]))
+        target = np.delete(target, index, axis=0)
+
+    return plane_list
