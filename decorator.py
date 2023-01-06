2e4e6e2b9024190195de849b34bc89d9a6a5dbfd
ray <397593251@qq.com>
2023-01-06 10:20:37 +0800

    add pointcloud

---
+++ decorator.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   decorator.py    
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/1/6 上午10:14   ray      1.0         None
+'''
+
+# import lib
+from functools import wraps
+import time
+import datetime
+
+
+def how_much_time(method):
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        start_time = datetime.datetime.now()
+        res = method(*args, **kwargs)
+        print(datetime.datetime.now() - start_time)
+        return res
+
+    return wrapper
+
+
+@how_much_time
+def test():
+    time.sleep(1)
+    a = 1 + 1
+
+
+if __name__ == '__main__':
+    test()
